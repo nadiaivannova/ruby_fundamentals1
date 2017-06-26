98c0d12cb896c8fe07779ba84c4a6c8c69090116
nadiaivannova <nadiaivannova@gmail.com>
2017-06-26 14:10:53 -0400

    strings excerice

--- exercise.rb
+++ exercise.rb
@@ -1,4 +1,13 @@
+# single line math
 puts 2 + 3
+# multy line ruby program
 puts 2
 puts 3
 puts 2 + 3
+# string sequence
+puts " put string"
+puts " Hello world"
+puts ' Hello world'
+
+puts " betty's pie shop"
+puts 'Betty\'s pie shop'
