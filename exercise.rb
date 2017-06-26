5eeb4107f82fc924cadf36139f37f1c62d16e3a4
nadiaivannova <nadiaivannova@gmail.com>
2017-06-26 14:18:58 -0400

    escape sequences

--- exercise.rb
+++ exercise.rb
@@ -8,6 +8,13 @@ puts 2 + 3
 puts " put string"
 puts " Hello world"
 puts ' Hello world'
-
+# double quotes
 puts " betty's pie shop"
 puts 'Betty\'s pie shop'
+#interpolation
+puts "Ada Lovelace lived for #{ 1852 - 1815 } years"
+#escape sequence
+puts " Hello \t\ tworld"
+puts " Hello \b\b\b\b\bGoodbye world"
+puts " Hello\rStart over world"
+puts "1. Hello\n2. World"
