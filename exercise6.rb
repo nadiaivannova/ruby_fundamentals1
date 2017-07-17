44287d238116271157bf3272869c458211ac070f
nadiaivannova <nadiaivannova@gmail.com>
2017-07-17 11:40:08 -0400

    exercise 6

---
+++ exercise6.rb
@@ -0,0 +1,7 @@
+#You decide to get some exercise and fresh air, but you want to keep track of how far from home you are.
+
+#Ask the user for input on what action to take - walk or run. If they walk, the total distance should go up by one, and you should update the user on their total distance traveled as follows:
+
+#{}"Distance from home is 6 km."
+
+#If they run, their total distance should go up by 5. Your program should keep asking for input - you don't know where you're going until you get there! Each time, you should print the total distance traveled.
